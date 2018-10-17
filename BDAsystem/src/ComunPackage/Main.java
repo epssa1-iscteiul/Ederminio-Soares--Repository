027bf3eb91e0c289d731763bd5c6bb2665efebb9
epssa1-iscteiul <epssa1@iscte-iul.pt>
2018-10-17 16:42:41 +0100

    versão1 commit

---
+++ BDAsystem/src/ComunPackage/Main.java
@@ -0,0 +1,100 @@
+package ComunPackage;
+import java.util.List;
+
+import com.restfb.Connection;
+import com.restfb.DefaultFacebookClient;
+import com.restfb.FacebookClient;
+import com.restfb.Version;
+import com.restfb.types.Group;
+import com.restfb.types.Post;
+import com.restfb.types.User;
+
+import ServerPackage.Server;
+
+public class Main {
+
+
+	public static void main(String[] args) {
+
+		/*String accessToken="EAAJJrQIZCZBh4BALPSuOp8atkzByndmyZCQDNUKEseJusV9ZBp7SgAZBBJubT8qHMCTemfjDwYZAjLsSVgn"
+				+ "UgVZBiHGEDqtHgL3eWt4gnCbmtNGFZBz3M1R7dZAsGiQCeoWo2GXBRfZBZAH0ZC8M7Hr1szeYAorMa8mqRdNfku2KtMR3Ey"
+				+ "QlwBXroOOdiCjsPzW7bTSWgGcNPhJHRAZDZD";
+
+		FacebookClient fclient=new DefaultFacebookClient(accessToken,Version.VERSION_3_1);
+		User me =fclient.fetchObject("me", User.class);
+		System.out.println(me.getId());
+
+
+		FacebookClient fbClient5 = new DefaultFacebookClient(accessToken,Version.VERSION_3_1);
+
+
+		Connection<Post> result = fbClient5.fetchConnection("me/feed",Post.class);
+		System.out.println("\nPosts:");
+		int counter5 = 0;
+		int counterTotal = 0;
+		for (List<Post> page : result) {
+			for (Post aPost : page) {
+				// Filters only posts that contain the word "Inform"
+				if (aPost.getMessage() != null ) {
+					System.out.println("---- Post "+ counter5 + " ----");
+					System.out.println("Id: "+"fb.com/"+aPost.getId());
+					System.out.println("Message: "+aPost.getMessage());
+					System.out.println("Created: "+aPost.getCreatedTime());
+					counter5++;
+				}
+				counterTotal++;
+			}
+		}
+		System.out.println("-------------\nNº of Results: " + counter5+"/"+counterTotal);	*/
+
+
+		
+		String accessToken="EAAJJrQIZCZBh4BALOugXFiGunDUIyZAZAAseTqzXbW89ulifM0DxmdjRVFoPyH8Jy"
+				+ "6H2BLyCovgLTMmmccnMZBuITKo045EUMNyAMLWiGEVd6Fo3NmYl6eBs8bME57QKWAcR6sJ4Q4ZCz"
+				+ "tQoWEVKsBdl4kjyCungMd1oxemZBfLnvuQJKLps7zHBTGFBqtTvF10I0Knx4fSyAZDZD";
+		
+		//String apptoken="643957369338398|jE5c3Lrv9BjHn0Fl11nlvVw40bI";
+
+		FacebookClient fclient=new DefaultFacebookClient(accessToken,Version.VERSION_3_1);
+		Connection<Group> groupfeed= fclient.fetchConnection("me/groups",Group.class);
+
+
+
+		for(List<Group> listGroup: groupfeed) {
+
+			for(Group group: listGroup) {
+				if(group.getName().equals("ISCTE - IUL")) {
+					System.out.println(group.getName());
+
+					Connection<Post> postfeed=fclient.fetchConnection(group.getId()+"/feed",Post.class);
+
+					for(List<Post> listPost: postfeed){
+
+						for(Post post: listPost) {
+
+							System.out.println(post.getFrom().getName());
+							System.out.println("-->"+post.getMessage());
+							System.out.println("fb.com/"+post.getId());
+
+						}
+
+					}
+
+				}
+			}
+		}
+
+
+
+
+
+
+
+
+
+	}
+
+
+
+
+}
---
+++ BDAsystem/src/UserPackage2/Token.java
@@ -0,0 +1,15 @@
+package UserPackage2;
+
+public class Token {
+
+	private String value;
+
+	public Token(String value) {
+		this.value=value;
+	}
+	
+	public String getValue() {
+		return value;
+	}
+
+}
